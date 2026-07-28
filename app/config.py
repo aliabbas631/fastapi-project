e94e34b5d734bd767e0e777966ddcad0b2285774
Ali Abbas <aliabbassj631@gmail.com>
2026-07-28 22:44:55 +0300

    new edit

--- app/config.py
+++ app/config.py
@@ -1,23 +1,26 @@
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+# class Settings(BaseSettings):
+#     database_hostname: str
+#     database_port: str
+#     database_password: str
+#     database_name: str
+#     database_username: str
+#     secret_key: str
+#     algorithm: str
+#     access_token_expire_minutes: int
 
-    class Config:
-        env_file = ".env"
+#     class Config:
+#         env_file = ".env"
 
-settings = Settings()
+# settings = Settings()
 
-class dburl(BaseSettings):
+class Dburl(BaseSettings):
     dburll: str
 
     class Config:
         env_file = ".env"
+
+
+dburl = Dburl()
